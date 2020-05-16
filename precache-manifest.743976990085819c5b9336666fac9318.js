380c585f25d6b4736da6fcfcd6778630575cc78c
Himanshu <gangwar0145@gmail.com>
2020-05-16 09:18:02 +0530

    Updates

---
+++ precache-manifest.743976990085819c5b9336666fac9318.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc6377a42ba20c13a44c99b5febf138d",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "2685548eae1fd1a9855e",
+    "url": "/react-todo/static/js/2.a327e769.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo/static/js/2.a327e769.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dc104b2529f7bfa978a",
+    "url": "/react-todo/static/js/main.ef3c27cd.chunk.js"
+  },
+  {
+    "revision": "af99325d0c658baba767",
+    "url": "/react-todo/static/js/runtime-main.56bbb7dd.js"
+  }
+]);
